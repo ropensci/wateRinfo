42910a4a72512261417d4136dbf51cffd2fc3ee2
stijnvanhoey <stijnvanhoey@gmail.com>
2017-10-18 17:20:40 +0200

    Add support for multiple base_urls

--- R/call_waterinfo.R
+++ R/call_waterinfo.R
@@ -1,17 +1,29 @@
 
 waterinfo_base <- function() "http://download.waterinfo.be/tsmdownload/KiWIS/KiWIS"
+waterinfo_pro_base <- function() "http://pro.waterinfo.be/tsmpro/KiWIS/KiWIS"
 
 #' Basic http call to waterinfo.be, providing error handling and json parsing
 #'
 #' @param query list of query options to be used together with the base string
+#' @param base_url str download | pro, default download defined
 #'
 #' @return waterinfo_api class object with content and info about call
 #'
 #' @export
 #' @importFrom httr GET http_type status_code http_error content
 #' @importFrom jsonlite fromJSON
-call_waterinfo <- function(query) {
-    res <- GET(waterinfo_base(), query = query)
+call_waterinfo <- function(query, base_url = "download") {
+
+    # check the base url, which depends of the query to execute
+    if (base_url == "download") {
+        base  = waterinfo_base()
+    } else if (base_url == "pro") {
+        base  = waterinfo_pro_base()
+    } else {
+        stop("Base url should be download or pro")
+    }
+
+    res <- GET(base, query = query)
 
     if (http_type(res) != "application/json") {
         stop("API did not return json", call. = FALSE)
@@ -22,7 +34,8 @@ call_waterinfo <- function(query) {
     if (http_error(res)) {
         stop(
             sprintf(
-                "Waterinfo API request failed [%s]\nWaterinfo %s: %s\nWaterinfo return message: %s",
+                "Waterinfo API request failed [%s]\nWaterinfo %s:
+                %s\nWaterinfo return message: %s",
                 status_code(res),
                 parsed$type,
                 parsed$code,
@@ -46,7 +59,6 @@ call_waterinfo <- function(query) {
 #' Custom print function of the API request response
 #'
 #' @param x waterinfo_api
-#' @param ...
 #'
 #' @export
 #'
