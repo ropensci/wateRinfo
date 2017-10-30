c263bfbf5d69b6dd6c1e98eb4a2dca30713f7f4d
stijnvanhoey <stijnvanhoey@gmail.com>
2017-10-30 12:12:13 +0100

    Remove mock test on timeseries

--- tests/testthat/test-timeseries_download.R
+++ tests/testthat/test-timeseries_download.R
@@ -1,22 +1,5 @@
 context("timeseries_download")
 
-# This does not call the waterinfo API, but a stored version of the response
-with_mock_API({
-    test_that("Check time series dataframe output format", {
-
-        pressure <- get_timeseries_tsid("78124042",
-                                        from = "2017-04-01",
-                                        to = "2017-04-02")
-
-        expect_is(pressure, "data.frame")
-        expect_equal(colnames(pressure),
-                     c("Timestamp", "Value", "Quality Code"))
-        expect_is(pressure$Timestamp, c("POSIXct", "POSIXt"))
-        expect_is(pressure$Value, c("numeric"))
-    })
-})
-
-
 test_that("Call works as expected", {
     skip_if_disconnected()
     skip_on_cran()
