813231c47538f192c707f27da2e6c6aeb264f170
stijnvanhoey <stijnvanhoey@gmail.com>
2017-10-30 09:37:51 +0100

    Add coverage empty frame

--- tests/testthat/test-timeseries_download.R
+++ tests/testthat/test-timeseries_download.R
@@ -1,7 +1,25 @@
 context("timeseries_download")
 
-test_that("works as expected", {
-    #skip_on_cran()
+# This does not call the waterinfo API, but a stored version of the response
+with_mock_API({
+    test_that("Check time series dataframe output format", {
+
+        pressure <- get_timeseries_tsid("78124042",
+                                        from = "2017-04-01",
+                                        to = "2017-04-02")
+
+        expect_is(pressure, "data.frame")
+        expect_equal(colnames(pressure),
+                     c("Timestamp", "Value", "Quality Code"))
+        expect_is(pressure$Timestamp, c("POSIXct", "POSIXt"))
+        expect_is(pressure$Value, c("numeric"))
+    })
+})
+
+
+test_that("Call works as expected", {
+    skip_if_disconnected()
+    skip_on_cran()
 
     pressure <- get_timeseries_tsid("78124042",
                                     from = "2017-04-01",
@@ -10,4 +28,21 @@ test_that("works as expected", {
     expect_is(pressure, "data.frame")
     expect_equal(colnames(pressure),
                  c("Timestamp", "Value", "Quality Code"))
+    expect_is(pressure$Timestamp, c("POSIXct", "POSIXt"))
+    expect_is(pressure$Value, c("numeric"))
+})
+
+
+test_that("Response of empty dataframe", {
+    skip_if_disconnected()
+    skip_on_cran()
+
+    pressure <- get_timeseries_tsid("78124042",
+                                    from = "1970-04-01",
+                                    to = "1970-04-02")
+
+    expect_is(pressure, "data.frame")
+    expect_equal(colnames(pressure),
+                 c("Timestamp", "Value", "Quality Code"))
+    expect_equal(nrow(pressure), 0)
 })
