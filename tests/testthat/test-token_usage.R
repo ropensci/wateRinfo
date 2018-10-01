3e2da537e27aba5086c7f7cca4444b70e7bf58a6
stijnvanhoey <stijnvanhoey@gmail.com>
2018-10-01 16:27:29 +0200

    use GMT for datetime test

--- tests/testthat/test-token_usage.R
+++ tests/testthat/test-token_usage.R
@@ -68,7 +68,8 @@ test_that("Print output of token is data type specific", {
   value <- "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3YjFjMDA4Ni05ZDUyLTQzOTAtYTM"
   token_url <- "http://download.waterinfo.be/kiwis-auth/token"
   token_type <- "Bearer"
-  expires <- as.POSIXlt("2018-10-02 15:13:28 CEST", tz = "CEST")
+
+  expires <- as.POSIXlt("2018-10-02 15:13:28 CEST", tz = "GMT")
 
   token_info <- c("Token:",
                   "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3YjFjMDA4Ni05ZDUyLTQzOTAtYTM",
